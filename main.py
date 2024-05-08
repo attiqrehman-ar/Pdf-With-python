1ec313d32171dddc0c1a75b979ad7218864742e5
Attiq30 <attiqrehman130@gmail.com>
2024-05-08 13:40:14 +0500

    Completed

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style ="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt= row["Topic"], align="L", ln=1, border=1)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
     pdf.line(10,21,200, 21 )
 
     pdf.ln(266)
@@ -26,6 +28,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
 
 
